38938c8b4482b05dc8fea15447775f2d8436c1af
opilane <opilane@HP-CZC2349HV2>
2025-05-14 14:27:11 +0300

    78

--- _21.py
+++ _21.py
@@ -17,7 +17,7 @@ def alusta(event):
     end_nupp.grid(row=17, column=3, pady=20, padx=10, rowspan=3, columnspan=3)
     stop_nupp.grid(row=18, column=3, pady=20, padx=10, rowspan=3, columnspan=3)
 
-def valik1()-> any:
+def valik()-> any:
     fail1="-cards.txt"
     with open (fail1, 'r', encoding="utf-8-sig") as f:
         c=[]
@@ -47,75 +47,93 @@ def valik1()-> any:
     h=g["k"]
     return c, h
 
-def valik2()-> any:
-    fail1="-cards.txt"
-    with open (fail1, 'r', encoding="utf-8-sig") as f:
-        c=[]
-        for rida in f:
-            c.append(eval(rida.strip()))
-    koik=0
-    fail="cards.txt"
-    with open(fail, 'r', encoding="utf-8-sig") as f:
-        cards=[]
-        for rida in f:
-            cards.append(eval(rida.strip()))
-    g=random.choice(cards)
-    card=["ch", "x", "b", "p"]
-    b=random.choice(card)
-    uus_card={'card': g[b]}
-    while 1:
-        if b in c:
-            g=random.choice(cards)
-            card=["ch", "x", "b", "p"]
-            b=random.choice(card)
-            uus_card={'card': g[b]}
-        else:
-            with open(fail1, 'a', encoding="utf-8-sig") as f:
-                f.write(str(uus_card)+'\n')
-            break
-    p=g[b]
-    k=g["k"]
-    return p,k
+
 
 def veel (event):
     summa=int(arv1)+int(arv2)+int(arv3)
     s=Label(aken, text=(f"Summa: {summa}"),  bg="white", font=("Arial", 17), fg="black",width=15, height=1 )
-    pilt_1.grid(row=15, column=0, pady=20, padx=10, rowspan=3, columnspan=3)
-    pilt_2.grid(row=15, column=1, pady=20, padx=10, rowspan=3, columnspan=3)
-    pilt_3.grid(row=15, column=2, pady=20, padx=10, rowspan=3, columnspan=3)
+    pilt_1.grid(row=15, column=0, pady=20, padx=10, rowspan=3, columnspan=1)
+    pilt_2.grid(row=15, column=1, pady=20, padx=10, rowspan=3, columnspan=1)
+    pilt_3.grid(row=15, column=2, pady=20, padx=10, rowspan=3, columnspan=1)
     s.grid(row=15, column=3, pady=5, padx=10, rowspan=3, columnspan=3)
     lisa_nupp.grid(row=16, column=3, pady=20, padx=10, rowspan=3, columnspan=3)
     end_nupp.grid(row=17, column=3, pady=20, padx=10, rowspan=3, columnspan=3)
+
+def end (event):
+    pilt_robot_1.grid(row=0, column=0, pady=20, padx=10, rowspan=3, columnspan=1)
+    pilt_robot_2.grid(row=0, column=1, pady=20, padx=10, rowspan=3, columnspan=1)
+    su.grid(row=0, column=3, pady=5, padx=10, rowspan=3, columnspan=3)
+    if summa < 21:
+        card_robot_3, arv_3=valik()
+        picture_robot_3=PhotoImage(file=card_robot_3)
+        pilt_robot_3=Label(aken,image=picture_robot_3 )
+        pilt_robot_3.grid(row=0, column=2, pady=20, padx=10, rowspan=3, columnspan=1)
+        pilt_robot_3.image= picture_robot_3
+        summa_t=summa_robot+int(arv_3)
+        su.config(text=(f"Summa: {summa_t}"))
+        if summa_t<21:
+            card_robot_4, arv_4=valik()
+            picture_robot_4=PhotoImage(file=card_robot_3)
+            pilt_robot_4=Label(aken,image=picture_robot_4 )
+            pilt_robot_4.grid(row=1, column=1, pady=20, padx=10, rowspan=3, columnspan=1)
+            pilt_robot_4.image= picture_robot_4
+            summa_q=summa_t+int(4)
+            su.config(text=(f"Summa: {summa_q}"))
+        if summa_t<21:
+            card_robot_5, arv_5=valik()
+            picture_robot_5=PhotoImage(file=card_robot_5)
+            pilt_robot_5=Label(aken,image=picture_robot_5 )
+            pilt_robot_5.grid(row=1, column=3, pady=20, padx=10, rowspan=3, columnspan=1)
+            pilt_robot_5.image= picture_robot_5
+            summa_s=summa_t+int(4)
+            su.config(text=(f"Summa: {summa_s}"))
+
+
+
+
 
 for i in range(3):
     aken.grid_rowconfigure(i, weight=1)
     aken.grid_columnconfigure(i, weight=1)
 
        
+login=
 
 nupp=Button(aken, text="Alusta mängu", bg="lightpink", font=("Arial", 20), fg="black", relief=RAISED, width=30)
 nupp.grid(row=1, column=1,pady=20)
 nupp.bind("<Button-1>", alusta)
 lisa_nupp=Button(aken, text="VEEL", bg="lightblue", font=("Arial", 12), fg="black", relief=RAISED, width=30)
+lisa_nupp.bind("<Button-1>", veel)
 end_nupp=Button(aken, text="LOPETA VOOR", bg="lightblue", font=("Arial", 12), fg="black", relief=RAISED, width=30)
+end_nupp.bind("<Button-1>", end)
 stop_nupp=Button(aken, text="STOP", bg="lightblue", font=("Arial", 12), fg="black", relief=RAISED, width=30 )
 
-card1, arv1=valik1()
-card2, arv2=valik2()
-card3, arv3=valik3()
-summa=int(arv1) + int(arv2)
+card1, arv1=valik()
+card2, arv2=valik()
+card3, arv3=valik()
+card_robot_1, arv_1=valik()
+card_robot_2, arv_2=valik()
+summa=int(arv1) + int(arv2) 
+summa_robot=int(arv_1) + int(arv_2)
+su=Label(aken, text=(f"Summa: {summa_robot}"),  bg="white", font=("Arial", 17), fg="black",width=15, height=1 )
 s=Label(aken, text=(f"Summa: {summa}"),  bg="white", font=("Arial", 17), fg="black",width=15, height=1 )
 picture1=PhotoImage(file=card1)
 picture2=PhotoImage(file=card2)
 picture3=PhotoImage(file=card3)
+picture_robot_1=PhotoImage(file=card_robot_1)
+picture_robot_2=PhotoImage(file=card_robot_2)
 pilt_1=Label(aken,image=picture1)
 pilt_2=Label(aken,image=picture2)
 pilt_3=Label(aken,image=picture3)
+pilt_robot_1=Label(aken,image=picture_robot_1 )
+pilt_robot_2=Label(aken,image=picture_robot_2 )
 pilt_1.image = picture1
 pilt_2.image = picture2
 pilt_3.image = picture3
+pilt_robot_1.image= picture_robot_1
+pilt_robot_2.image= picture_robot_2
 
-photo=PhotoImage()
+
 # distroe
 aken.mainloop()
 
